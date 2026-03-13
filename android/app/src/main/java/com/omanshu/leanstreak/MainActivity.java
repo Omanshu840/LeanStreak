2cd09de75b7b44c4c20cd50ae82bc0ab0d0d92b5
Omanshu Mahawar <omanshumahawar.181co237@nitk.edu.in>
2026-03-13 22:33:48 +0530

    add capacitor

---
+++ android/app/src/main/java/com/omanshu/leanstreak/MainActivity.java
@@ -0,0 +1,5 @@
+package com.omanshu.leanstreak;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.omanshu.leanstreak',
+  appName: 'LeanStreak',
+  webDir: 'out'
+};
+
+export default config;
